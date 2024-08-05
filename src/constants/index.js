c7aa21a64c1edae223f56070e87ac04e704dbd0f
MohammadShabazuddin <44221567+MohammadShabazuddin@users.noreply.github.com>
2024-08-04 23:19:11 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,5 +4,5 @@ export const REQUEST_TOKEN = 'request_token';
 export const THEME_MODE = 'theme_mode';
 export const TMDB_IMAGE_PATH = 'https://image.tmdb.org/t/p';
 export const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
-export const BLUE_LOGO = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
-export const RED_LOGO = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
+export const BLUE_LOGO = 'https://fontmeme.com/permalink/240804/4dc0a92a0a7bc888dec44198c7c99c68.png';
+export const RED_LOGO = 'https://fontmeme.com/permalink/240804/3769e621d913b19ee86b85ab3fd3dad7.png';
